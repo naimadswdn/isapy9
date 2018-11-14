99dd05b9f1652cb4ed3c54f696c0f6686e59294b
naimadswdn <naimadswdn@gmail.com>
2018-11-14 20:30:37 +0100

    Pierwsza wersja skryptu pisanego na zajęciach.

--- dzien_1.py
+++ dzien_1.py
@@ -7,7 +7,10 @@ last_sign = name[signs - 1] # can be replaced with name[-1:]
 
 
 def hello_function(t):
-    print(f'Cześć {t.capitalize()}!')
+    if last_sign=='a':
+        print(f'Cześć {t.capitalize()}! Jesteś kobietą.')
+    else:
+        print(f'Cześć {t.capitalize()}! Jesteś mężczyzną.')
     print(f'Twoje imię ma {signs} znaków.')
     print(f'Ostatnia litera imienia to {last_sign}.')
 
