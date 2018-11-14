b231a6d2a1618a7a9c01631ac9a19f8977fdf1d1
naimadswdn <naimadswdn@gmail.com>
2018-11-14 20:20:35 +0100

    Improvements to 'Say Hello' program

--- dzien_1.py
+++ dzien_1.py
@@ -1,10 +1,15 @@
 # Script that is going to say 'Hello' to you!
 
 name = input("Podaj swoje imię:\n")
+name = name.strip(' ')  # removing spaces from user string
+signs = len(name)
+last_sign = name[signs - 1] # can be replaced with name[-1:]
 
 
 def hello_function(t):
     print(f'Cześć {t.capitalize()}!')
+    print(f'Twoje imię ma {signs} znaków.')
+    print(f'Ostatnia litera imienia to {last_sign}.')
 
 
 hello_function(name)
