f43a70a69a1e7d40914ea8319c8032254218c856
naimadswdn <naimadswdn@gmail.com>
2018-11-18 13:11:22 +0100

    Improving 6th task to check if binary number is only six digit lenght

--- dzien_2/zad_6.py
+++ dzien_2/zad_6.py
@@ -5,8 +5,10 @@ import time
 print('Hello! This program is going to convert 6 digit binary number to decimal number.')
 time.sleep(2)
 
+
+
 while True:
-    binary = input('Please provide binary number with exactly 6 digits: ')
+    binary = input('Please provide binary number with exactly 6 digits and using only 0/1 numbers: ')
     if len(binary) != 6:
         print('Your number is not 6 digits! Please correct it. ')
         continue
@@ -21,7 +23,10 @@ fourth = 8
 fifth = 16
 sixth = 32
 
-decimal = int(binary[-1])*first + int(binary[-2])*second + int(binary[-3])*third + int(binary[-4])*fourth + int(binary[-5])*fifth + int(binary[-6])*sixth
-
+decimal = int(binary[-1]) * first + int(binary[-2]) * second + int(binary[-3]) * third + int(binary[-4]) * fourth + int(
+        binary[-5]) * fifth + int(binary[-6]) * sixth
 
 print(f'Binary {binary} is equal to {decimal} in decimal system.')
+
+
+
