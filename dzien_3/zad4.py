dbc761ddea4ca19f411404dadc9d04b4e66bca20
naimadswdn <naimadswdn@gmail.com>
2018-11-22 19:59:12 +0100

    Initial version of 4th task from homework.

---
+++ dzien_3/zad4.py
@@ -0,0 +1,24 @@
+# Program to calculate how old is dog in their age scale
+
+scale = [10.5, 4]
+
+
+while True:
+    age = input('Please provide number of dog years in human scale: ')
+    try:
+        age = float(age)
+        break
+    except:
+        print('It is not a number! Please provide correct number.')
+        continue
+
+def dog_years():
+    if age <= 2:
+        dog_age = age * scale[0]
+        print (f"Your dog's age in dog scale is {dog_age} years.")
+    else:
+        dog_age = 2 * scale[0] + (age-2)*scale[1]
+        print (f"Your dog's age in dog scale is {dog_age} years.")
+
+
+dog_years()
