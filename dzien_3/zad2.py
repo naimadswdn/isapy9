1ca34dfe4b6065e5f908a83b4f97581434985a75
naimadswdn <naimadswdn@gmail.com>
2018-11-23 18:37:21 +0100

    Working version of 2nd task added.

--- dzien_3/zad2.py
+++ dzien_3/zad2.py
@@ -5,26 +5,25 @@ coins = [5, 2, 1, 0.5, 0.2, 0.1]
 money = input('Please provide amount of money to calculate remainder: ')
 money = float(money)
 
-# for i in coins:
-#     check_modulo = lambda x : x % i
-#
-#     if check_modulo(money) == 0:
-#         amount = money / i
-#         print(f'{i}: {amount}')
-#         if amount * i == money:
-#             break
-#         else:
-#             continue
-#     else:
-#         continue
 
 for i in coins:
-    check_modulo = lambda x : x % i
-    check_dev = lambda  x : x / i
-    if check_modulo(money) == 0 and check_dev(money) !=0:
-        print(f'{i}: {check_dev(money)}')
-        money = money - i*check_dev(money)
-    elif check_modulo(money) != 0 and check_dev(money) !=0:
-        pass
+    check_modulo = lambda x : round(x % i,1)
+    check_div = lambda  x : x / i
+    if check_modulo(money) == 0 and check_div(money) !=0:
+        # print('ahoj') just to show if the if block was used
+        print(f'{i}: {check_div(money)}')
+        money = money - i*check_div(money)
+    elif check_modulo(money) != 0 and check_div(money) !=0:
+        div_floor = lambda  x : x //i
+        print(f'{i}: {div_floor(money)}')
+        # print(check_modulo(money)) #just to check current number
+        money = check_modulo(money)
+    elif check_modulo(money) == 0 and check_div(money) ==0:
+        break
+    elif check_modulo(money) != 0 and check_div(money) ==0:
+        print('Impossible condition. How did you call it?!')
+        break
+
+
 
 
