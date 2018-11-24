787990f995c4c87d15effb9ffa61849ff316672d
naimadswdn <naimadswdn@gmail.com>
2018-11-24 14:01:51 +0100

    Adding try block, 0 value information and format results (left aligned).

--- dzien_3/zad2.py
+++ dzien_3/zad2.py
@@ -2,27 +2,36 @@
 
 coins = [5, 2, 1, 0.5, 0.2, 0.1]
 
-money = input('Please provide amount of money to calculate remainder: ')
-money = float(money)
+while True:
+    money = input('Please provide amount of money to calculate remainder: ')
+    try:
+        money = float(money)
+        break
+    except:
+        print('Wrong cash amount. Please correct it! ')
+        continue
 
-
-#TO DO: not 0 ammount/ align/ def?
 
 for i in coins:
     check_modulo = lambda x : round(x % i,1)
     check_div = lambda  x : x / i
-    if check_modulo(money) == 0 and check_div(money) !=0:
+    if check_modulo(money) == 0 and check_div(money) != 0:
         # print('ahoj') just to show if the if block was used
-        print(f'{i}: {check_div(money)}')
+        print(f'{i:>3} PLN: {check_div(money)}')
         money = money - i*check_div(money)
-    elif check_modulo(money) != 0 and check_div(money) !=0:
+        if money == 0:
+            break
+    elif check_modulo(money) != 0 and check_div(money) != 0:
         div_floor = lambda  x : x //i
-        print(f'{i}: {div_floor(money)}')
+        print(f'{i:>3} PLN: {div_floor(money)}')
         # print(check_modulo(money)) #just to check current number
         money = check_modulo(money)
-    elif check_modulo(money) == 0 and check_div(money) ==0:
+        if money == 0:
+            break
+    elif check_modulo(money) == 0 and check_div(money) == 0:
+        print('You have typed 0 as cash amount!')
         break
-    elif check_modulo(money) != 0 and check_div(money) ==0:
+    elif check_modulo(money) != 0 and check_div(money) == 0:
         print('Impossible condition. How did you call it?!')
         break
 
